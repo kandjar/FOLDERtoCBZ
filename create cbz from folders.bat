c88e3e9c0b28ebfcaf68cc9293dd7e21e6dbcc78
kandjar <kandjar@users.noreply.github.com>
2022-04-06 12:09:16 +0200

    Update create cbz from folders.bat

--- create cbz from folders.bat
+++ create cbz from folders.bat
@@ -1,3 +1,4 @@
 @Echo Off 
-FOR /D /r %%G in (*) DO "C:\Program Files\7-Zip\7z.exe" a -tzip "%%~nxG.cbz" "%%~nxG\*.*"  &:: Don't forget to change your drive name if 7zip is not stored at "C:\"
+:: Don't forget to change your drive name if 7zip is not stored at "C:\"
+FOR /D /r %%G in (*) DO "C:\Program Files\7-Zip\7z.exe" a -tzip "%%~nxG.cbz" "%%~nxG\*.*"
 ::FOR /D /r %%G in (*) DO RMDIR /S /Q "%%~nxG"
